2e6863020314d85f6eb2ffb7b726dab1f69ddf15
Joseph Iglecias <igleciasjoseph@gmail.com>
2019-06-06 15:58:59 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var li = ['Learn React', 'Climb Mt.Everest', 'Run a marathon', 'Feed the dogs']
+  const items = [];
+  for (const [index, value] of li.entries()) {
+    items.push(<li value={index}>{value}</li>);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <h3>Things I need to do:</h3>
+      <div>
+        {items}
+      </div>
     </div>
   );
 }
